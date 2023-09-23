fbd521d6ca2a8f8a2782206188a034ecea3de3b7
Zhongfa Wang <Zhongfa_Wang@outlook.com>
2023-09-23 02:14:03 +0000

    Correct an error

--- http-run.sh
+++ http-run.sh
@@ -2,6 +2,6 @@
 echo "The results when $1% fences are inserted: " >> /home/zhongfa/benchmarks/run/results/results-http.log
 cd /home/zhongfa/benchmarks/run/bench
 # seq 1 10 |sudo parallel -j10 -k "cd ./http-parser-{} && ./$2 large.log >> /home/zhongfa/benchmarks/run/results/results-http.log"
-seq 1 10 |sudo parallel -j10 -k "cd ./http-parser-{} && (time ./$2 large.log) 2>> /home/zhongfa/benchmarks/run/results/results-http.log"
+seq 1 10 |sudo parallel -j10 -k "cd ./http-parser-{} && (time ./$2 large.txt) 2>> /home/zhongfa/benchmarks/run/results/results-http.log"
 echo "The results when $1% fences done" >> /home/zhongfa/benchmarks/run/results/results-http.log
 
